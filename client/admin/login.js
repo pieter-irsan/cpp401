304b17001a532e9964338670cdddc8e2b40799e4
pieter-irsan <pieterirsan.pi@gmail.com>
2022-10-18 00:28:21 +0700

    admin login api in progress

--- client/admin/login.js
+++ client/admin/login.js
@@ -11,17 +11,18 @@ function loginAdmin() {
 	}));
 
 	req.onreadystatechange = function() {
-        console.log(this.responseText)
+		console.log(this.readyState, this.status)
         if (this.readyState == 4 && this.status == 200) {
             // const [response] = JSON.parse(this.responseText);
             // const token = response['title']
 			// localStorage.setItem('token', token);
-			return location.href = 'file:///C:/Users/piete/Documents/Projects/cpp401/client/admin/index.html';
+			window.setTimeout(() => window.location.href = 'file:///C:/Users/piete/Documents/Projects/cpp401/client/admin/index.html', 1000);
+			// return location.href = 'file:///C:/Users/piete/Documents/Projects/cpp401/client/admin/index.html';
 		} else if (this.readyState == 4 && this.status == 401) {
 			alert("Incorrect username/password!");
 			location.reload();
-        } else {
-			alert("Login attempt failed!");
+        } else if (this.readyState == 4 && this.status == 500) {
+			alert("500 — Internal Server Error");
 			location.reload();
 		}
     }
--- server/admin/auth/api.js
+++ server/admin/auth/api.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const api = express.Router();
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const Pool = require('pg').Pool;
 const pool = new Pool({
@@ -14,8 +14,12 @@ const pool = new Pool({
 api.post('/login', function(request, response) {
     const { username, password } = request.body;
     pool.query(`select exists(select * from "user" where user_type = 'admin' and username = $1 and password = $2)`, [username, password], (error, result) => {
+        jwt.sign({
+            username: username,
+            password: password
+        }, jwtSecret)
         if (error) response.status(500).json({ error: error });
-        else if (!result.rows.exists) response.status(401);
+        else if (!result.rows[0].exists) response.status(401).json(result.rows[0].exists);
         else response.status(200).json(result.rows);
     });
 });
